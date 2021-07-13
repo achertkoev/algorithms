e6085ee9b4efd4c5c48ea846dbbb9419bc8034ac
Albert Chertkoev <Betak6630@gmail.com>
2021-07-13 18:11:51 +0300

    Added QuickFind algorithm

---
+++ Algorithms/QuickFindUF.cs
@@ -0,0 +1,39 @@
+﻿namespace Algorithms
+{
+    /// <summary>
+    /// </summary>
+    public class QuickFindUF
+    {
+        public QuickFindUF(int n)
+        {
+            Ids = new int[n];
+            for (var i = 0; i < Ids.Length; i++)
+                Ids[i] = i;
+        }
+
+        private int[] Ids { get; }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public bool IsConnected(int p, int q)
+        {
+            return Ids[p] == Ids[q];
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        public void Union(int p, int q)
+        {
+            var pId = Ids[p];
+            var qId = Ids[q];
+            for (var i = 0; i < Ids.Length; i++)
+                if (Ids[i] == pId)
+                    Ids[i] = qId;
+        }
+    }
+}
